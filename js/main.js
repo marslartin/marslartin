542648f095ed6095ece64bab06ec5365fad8fa3e
Lars Martin Sandbakken <larsms@me.com>
2022-02-12 03:05:46 +0000

    first update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("logo span.typed").typed({
+        strings: ["instances", "droplets", "servers"],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 700
+    });
+]
